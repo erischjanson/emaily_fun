b8abea2ca1bfcbe6da1986629c561a42ec4ea5f4
Elizabeth <erischjanson@gmail.com>
2018-07-03 15:37:52 -0500

    oath working

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'su'});
-});
+require('./routes/authRoutes')(app);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
